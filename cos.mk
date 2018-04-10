255aefb6b699692cf84a453464e4da8ab2b4a2b8
Sagar Shah <sagarshah2326@gmail.com>
2018-04-10 08:10:43 -0400

    berkeley: Initial Cosmic-OS support
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- cos.mk
+++ cos.mk
@@ -16,10 +16,10 @@
 
 $(call inherit-product, device/huawei/berkeley/full_berkeley.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Cosmic-OS stuff.
+$(call inherit-product, vendor/cos/common.mk)
 
-PRODUCT_NAME := lineage_berkeley
+PRODUCT_NAME := cos_berkeley
 PRODUCT_DEVICE := berkeley
 PRODUCT_BRAND := Huawei
 PRODUCT_MODEL := Honor View 10
